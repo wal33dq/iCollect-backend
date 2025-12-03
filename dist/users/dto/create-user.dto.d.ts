357896beb02e0d9affc8986459b2459c1c8b544f
Waleed Qureshi <83373227+wal33dq@users.noreply.github.com>
2025-12-03 20:51:25 +0500

    chore: stop tracking dist folder

--- dist/users/dto/create-user.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserRole } from '../schemas/user-role.enum';
-export declare class CreateUserDto {
-    fullName: string;
-    username: string;
-    email: string;
-    password: string;
-    role: UserRole;
-}
--- dist/users/dto/update-user.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserRole } from '../schemas/user-role.enum';
-export declare class UpdateUserDto {
-    fullName?: string;
-    username?: string;
-    email?: string;
-    password?: string;
-    role?: UserRole;
-}
